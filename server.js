4499703c10efc0fc53c7fcca7a70d5d4642edee8
felipebrasil8 <felipebrasil8@gmail.com>
2019-05-25 00:25:25 +0000

    alteracao ip docker

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express')
     db = require('./config/database'),
-	ip = '142.93.204.36';
+    ip = '0.0.0.0';
 
 http.createServer(app).listen(3000, ip, function() {
     console.log('API escutando na porta: ' + this.address().port);
